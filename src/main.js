8f26ce847f65df4919ad6575f80fbc74b20b3333
Onur BIYIK <onurbiyik@gmail.com>
2021-04-30 10:58:01 +0300

    vue confıg

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+fetch('https://localhost:44369/whoami', { credentials: 'include', method: 'post' })
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
@@ -16,3 +16,5 @@ export default {
   }
 }
 </script>
+
+
